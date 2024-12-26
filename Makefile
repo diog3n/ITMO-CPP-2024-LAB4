9bc8b2afbd86347fbac371dd511d73c2f684cfea
Anufriev Ilia <ilia.anufriev@infotecs.ru>
2024-12-26 12:34:09 +0300

    Added better tests and new operations

---
+++ Makefile
@@ -0,0 +1,2 @@
+default:
+	g++ src/*.cpp -std=c++17 -g -o sparse_matrix
--- src/main.cpp
+++ src/main.cpp
@@ -1,394 +1,190 @@
 #include "smatrix.hpp"
 #include "iostream"
+#include "cassert"
+
+#define TEST_LABEL_VAR_NAME __test_label__
+
+#define TEST(__label) \
+        { const char *TEST_LABEL_VAR_NAME = #__label;
+
+#define END_TEST \
+        std::cout << "Test " << TEST_LABEL_VAR_NAME << " OK!" << std::endl; }
 
 int main(int argc, char **argv) {
-	SparseVector<int> svec1(5);
+        TEST(matrix equality) {
+                SparseMatrix mat1(3, 5);
+                SparseMatrix mat2(3, 5);
 
-	for (int i = 0; i < svec1.size(); i++) {
-		std::cout << svec1.Get(i) << std::endl;
-	}
+                mat1.Set(0, 1, 3);
+                mat1.Set(0, 2, 3);
 
-	std::cout << std::endl;
+                mat2.Set(0, 1, 2);
+                mat2.Set(0, 3, 1);
 
-	for (int val : svec1) {
-		std::cout << val << std::endl;
-	}
+                assert(mat1 != mat2);
 
-	std::cout << std::endl;
+                SparseMatrix mat3(3, 5);
+                SparseMatrix mat4(3, 5);
 
-	SparseVector<int> svec2(5);
+                mat3.Set(0, 1, 3);
+                mat3.Set(0, 2, 3);
 
-	for (int i = 0; i < svec2.size(); i++) {
-		std::cout << svec2.Get(i) << std::endl;
-	}
+                mat4.Set(0, 1, 3);
+                mat4.Set(0, 2, 3);
 
-	std::cout << std::endl;
-
-	{
-		SparseMatrix mat1(3, 5);
-		SparseMatrix mat2(3, 5);
-
-		mat1.Set(0, 1, 3);
-		mat1.Set(0, 2, 3);
-
-		mat2.Set(0, 1, 2);
-		mat2.Set(0, 3, 1);
-
-		if (mat1 == mat2) std::cout << "equal" << std::endl;
-		else std::cout << "not equal" << std::endl;
-
-		for (const SparseMatrix::row_type& row : mat1) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
+                assert(mat3 == mat4);
 
-		for (const SparseMatrix::row_type& row : mat2) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-
-		SparseMatrix mat3 = mat1 + mat2;
-
-		for (const SparseMatrix::row_type& row : mat3) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-	}
-
-	{
-		SparseMatrix mat1(2, 2);
-		SparseMatrix mat2(2, 2);
-
-		mat1.Set(0, 0, 1);
-		mat1.Set(0, 1, 2);
-		mat1.Set(1, 0, 3);
-		mat1.Set(1, 1, 4);
-
-		mat2.Set(0, 0, 1);
-		mat2.Set(0, 1, 2);
-		mat2.Set(1, 0, 3);
-		mat2.Set(1, 1, 4);
-
-		for (const SparseMatrix::row_type& row : mat1) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-
-		for (const SparseMatrix::row_type& row : mat2) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-
-		auto result = mat1 * mat2;
-
-		for (const SparseMatrix::row_type& row : result) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-	}
-
-	{
-		SparseMatrix mat1 = {
-			{ 1, 0, 1 },
-			{ 2, 1, 1 },
-			{ 0, 1, 1 },
-			{ 1, 1, 2 },
-		};
-
-		SparseMatrix mat2 = {
-			{ 1, 2, 1 },
-			{ 2, 3, 0 },
-			{ 4, 2, 2 }
-		};
-
-		for (const SparseMatrix::row_type& row : mat1) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-
-		for (const SparseMatrix::row_type& row : mat2) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-
-		auto result = mat1 * mat2;
-
-		for (const SparseMatrix::row_type& row : result) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-	}
-
-	{
-		SparseMatrix mat1 = {
-			{ 1, 0, 1 },
-			{ 2, 1, 1 },
-			{ 0, 1, 1 },
-			{ 1, 1, 2 },
-		};
-
-		SparseMatrix mat2 = {
-			{ 1 },
-			{ 2 },
-			{ 4 }
-		};
-
-		for (const SparseMatrix::row_type& row : mat1) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-
-		for (const SparseMatrix::row_type& row : mat2) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-
-		auto result = mat1 * mat2;
-
-		for (const SparseMatrix::row_type& row : result) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-	}
-
-	{
-		SparseMatrix mat1 = {
-			{ 1, 0, 1 },
-			{ 2, 1, 1 },
-			{ 0, 1, 1 },
-		};
-
-		for (const SparseMatrix::row_type& row : mat1) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-
-		SparseMatrix mat2 = mat1.SubMatrix(1, 1);
-
-		for (const SparseMatrix::row_type& row : mat2) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-	}
-
-	{
-		SparseMatrix mat1 = {
-			{ 1, 2, 3 },
-			{ 4, 5, 6 },
-			{ 7, 8, 9 },
-		};
-
-		for (const SparseMatrix::row_type& row : mat1) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-
-		std::cout << "Determinant = " << mat1.Determinant() << std::endl;
-	}
-
-	{
-		SparseMatrix mat1 = {
-			{ 4,   3,   2,  2 },
-			{ 0,   1,  -3,  3 },
-			{ 0,  -1,   3,  3 },
-			{ 0,   3,   1,  1 }
-		};
-
-		for (const SparseMatrix::row_type& row : mat1) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-
-		std::cout << "Determinant = " << mat1.Determinant() << std::endl;
-	}
-
-	{
-		SparseMatrix mat1 = {
-			{ 4, 5, 2, 5, 1 },
-			{ 1, 4, 3, 1, 0 },
-			{ 2, 1, 1, 3, 5 },
-			{ 2, 3, 1, 4, 5 },
-			{ 12, 1, 3, 4, 2}
-		};
-
-		for (const SparseMatrix::row_type& row : mat1) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-
-		std::cout << "Determinant = " << mat1.Determinant() << std::endl;
-	}
-
-	{
-		SparseMatrix mat1 = {
-			{ 4, 5, 2, 5, 1 },
-			{ 1, 4, 3, 1, 0 },
-			{ 2, 1, 1, 3, 5 },
-			{ 2, 3, 1, 4, 5 },
-			{ 12, 1, 3, 4, 2}
-		};
-
-		mat1.RowOperation(0, 2, SparseMatrix::ADD);
-
-		for (const SparseMatrix::row_type& row : mat1) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-	}
-
-	{
-		SparseMatrix mat1 = {
-			{ 4, 5, 2, 5, 1 },
-			{ 1, 4, 3, 1, 0 },
-			{ 2, 1, 1, 3, 5 },
-			{ 2, 3, 1, 4, 5 },
-			{ 12, 1, 3, 4, 2}
-		};
-
-		mat1.ColOperation(0, 2, SparseMatrix::ADD);
-
-		for (const SparseMatrix::row_type& row : mat1) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-	}
-
-	{
-		SparseMatrix mat1 = MakeIdentityMatrix<double>(5);
-
-		for (const SparseMatrix::row_type& row : mat1) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-	}
-
-	{
-		SparseMatrix mat1 = MakeIdentityMatrix<double>(5);
-
-		for (const SparseMatrix::row_type& row : mat1) {
-			for (int elem : row) {
-				std::cout << elem << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << std::endl;
-	}
-
-	{
-		SparseMatrix mat1 = {
-			{ 2, 1 },
-			{ 7, 4 },
-		};
-
-		auto inv = mat1.Inverse();
-
-		std::cout << mat1 << std::endl;
-		std::cout << inv << std::endl;
-
-		std::cout << mat1 * inv << std::endl;
-
-	}
-
-	{
-		SparseMatrix mat1 = {
-			{ 4, 5, 2, 5, 1 },
-			{ 1, 4, 3, 1, 0 },
-			{ 2, 1, 1, 3, 5 },
-			{ 2, 3, 1, 4, 5 },
-			{ 12, 1, 3, 4, 2}
-		};
-
-
-		auto inv = mat1.Inverse();
-
-		std::cout << mat1 << std::endl;
-		std::cout << inv << std::endl;
-
-        auto res = mat1 * inv;
-
-		std::cout << mat1 * inv << std::endl;
-	}
-
-	return 0;
+                END_TEST;
+        }
+
+        TEST(matrix sum) {
+                SparseMatrix mat1 = {
+                        { 1, 2, 3 },
+                        { 3, 4, 5 },
+                };
+
+                SparseMatrix mat2 = {
+                        { 2, 3, 4 },
+                        { 4, 5, 6 },
+                };
+
+                SparseMatrix sum = {
+                        { 3, 5, 7  },
+                        { 7, 9, 11 }
+                };
+
+                assert((mat1 + mat2) == sum);
+
+                END_TEST;
+        }
+
+        TEST(matrix diff) {
+                SparseMatrix mat1 = {
+                        { 1, 2, 3 },
+                        { 3, 4, 5 },
+                };
+
+                SparseMatrix mat2 = {
+                        { 2, 3, 4 },
+                        { 4, 5, 6 },
+                };
+
+                SparseMatrix diff = {
+                        { -1, -1, -1 },
+                        { -1, -1, -1 }
+                };
+
+                assert((mat1 - mat2) == diff);
+
+                END_TEST;
+        }
+
+        TEST(matrix multiplication) {
+                SparseMatrix mat1 = {
+                        { 3, 2 },
+                        { 1, 4 },
+                        { 5, 3 }
+                };
+
+                SparseMatrix mat2 = {
+                        { 3, 2 },
+                        { 1, 4 }
+                };
+
+                SparseMatrix result = {
+                        { 11, 14 },
+                        { 7,  18 },
+                        { 18, 22 }
+                };
+
+                assert((mat1 * mat2) == result);
+
+                END_TEST;
+        }
+
+        TEST(matrix-vector multiplication) {
+                SparseMatrix mat = {
+                        { 1, -1, 2 },
+                        { 0, -3, 1 }
+                };
+
+                SparseVector<double> vec = { 2, 1, 0 };
+
+                SparseMatrix result = {
+                        { 1  },
+                        { -3 }
+                };
+
+                assert((mat * vec) == result);
+
+                END_TEST;
+        }
+
+        TEST(matrix determinant) {
+                SparseMatrix mat1 = {
+                        { 1, 2, 3 },
+                        { 4, 5, 6 },
+                        { 7, 8, 9 },
+                };
+
+                assert(mat1.Determinant() == 0);
+
+                SparseMatrix mat2 = {
+                        { 4,   3,   2,  2 },
+                        { 0,   1,  -3,  3 },
+                        { 0,  -1,   3,  3 },
+                        { 0,   3,   1,  1 }
+                };
+
+                assert(mat2.Determinant() == -240);
+
+                SparseMatrix mat3 = {
+                        { 4, 5, 2, 5, 1 },
+                        { 1, 4, 3, 1, 0 },
+                        { 2, 1, 1, 3, 5 },
+                        { 2, 3, 1, 4, 5 },
+                        { 12, 1, 3, 4, 2}
+                };
+
+                assert(mat3.Determinant() == 503);
+
+                END_TEST;
+        }
+
+        TEST(matrix inverse) {
+                SparseMatrix mat1 = {
+                        { 2, 1 },
+                        { 7, 4 },
+                };
+
+                SparseMatrix result = {
+                        { 4, -1 },
+                        { -7, 2 }
+                };
+
+                assert(result == mat1.Inverse());
+
+                SparseMatrix mat2 = {
+                        { 4, 5, 2, 5, 1 },
+                        { 1, 4, 3, 1, 0 },
+                        { 2, 1, 1, 3, 5 },
+                        { 2, 3, 1, 4, 5 },
+                        { 12, 1, 3, 4, 2}
+                };
+
+                SparseMatrix identity = {
+                        { 1, 0, 0, 0, 0 },
+                        { 0, 1, 0, 0, 0 },
+                        { 0, 0, 1, 0, 0 },
+                        { 0, 0, 0, 1, 0 },
+                        { 0, 0, 0, 0, 1 },
+                };
+
+                auto inv = mat2.Inverse();
+
+                assert(SparseMatrix(mat2 * inv) == identity);
+                END_TEST;
+        }
+
+        return 0;
 }
